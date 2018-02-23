cbd0ad2c583ee1d9ed510c4d5eef14ad76e3d6a8
KatarzynaZych <korzonek.katarzyna@gmail.comgit config --global user.name KatarzynaZych>
2018-02-23 15:33:38 +0000

    zadanie 17.4

--- src/main/java/com/crud/tasks/repository/DbService.java
+++ src/main/java/com/crud/tasks/repository/DbService.java
@@ -3,8 +3,6 @@ package com.crud.tasks.repository;
 import com.crud.tasks.domain.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.validation.constraints.Null;
 import java.util.List;
 import java.util.Optional;
 
