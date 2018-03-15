fee9171759516cff8c245f0a6e560d9ff7fe1da8
KatarzynaZych <korzonek.katarzyna@gmail.comgit config --global user.name KatarzynaZych>
2018-03-15 12:25:42 +0000

    rozdzial 18

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,8 +1,10 @@
-package com.crud.tasks.repository;
+package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
